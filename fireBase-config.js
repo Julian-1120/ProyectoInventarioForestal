c43fe74e1f8297e1342ed8624c59de05e323e602
Julian-1120 <jmacias7@udi.edu.co>
2025-10-14 20:14:00 -0500

    cambio de nuevo en StorageBucket

--- fireBase-config.js
+++ fireBase-config.js
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCzA3mTRkGlNNNpspzps7RBUFCe-KiZPmM",
   authDomain: "proyecto7l.firebaseapp.com",
   projectId: "proyecto7l",
-  storageBucket: "proyecto7l.appspot.comp",
+  storageBucket: "proyecto7l.firebasestorage.app",
   messagingSenderId: "682157927077",
   appId: "1:682157927077:web:f8eddbda29d7eee596bb4c",
   measurementId: "G-FB90YY2DZH",
